11232cbf97d5cb43a1bf1a164b3af61243312a26
Repli1 <5724@holbertonstudents.com>
2023-01-11 16:48:10 -0800

    Add task 8

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_postorder - Traverses the tree in postorder
+ * calling a function along the way.
+ * @tree: The root of the tree to traverse.
+ * @func: The function to apply to the node.
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL)
+		return;
+	if (tree->left)
+		binary_tree_postorder(tree->left, func);
+	if (tree->right)
+		binary_tree_postorder(tree->right, func);
+	func(tree->n);
+}
+
