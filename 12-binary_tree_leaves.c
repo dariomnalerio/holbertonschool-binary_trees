55a3356ca101770cb917145b7f7aa8b3e9c9b194
dariomnalerio <dariomnalerio@gmail.com>
2023-01-13 09:48:51 -0800

    Task 12 reimplementation

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,26 +7,11 @@
 */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t count = 0, left = 0, right = 0;
-
 	if (!tree)
 		return (0);
-	if (tree->left)
-	{
-		binary_tree_leaves(tree->left);
-		left++;
-	}
-	if (tree->right)
-	{
-		binary_tree_leaves(tree->right);
-		right++;
-	}
-
-	count = left + right;
-	if (!tree->left && !tree->right)
-		count++;
-
-	return (count);
+	if (!tree->right && !tree->left)
+		return (1);
+	else
+		return (binary_tree_leaves(tree->left) +
+			binary_tree_leaves(tree->right));
 }
-
-
